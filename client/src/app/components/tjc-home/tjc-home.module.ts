1661869cbcc1449407c1b9067a6e4b60a7e47fb6
DChatterjee321 <Debojyoti.Chatterjee@prolifics.com>
2022-05-04 16:03:59 +0530

    UI structure 001

---
+++ client/src/app/components/tjc-home/tjc-home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TjcHomeComponent } from './tjc-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    TjcHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TjcHomeModule { }
